ac625bcaf37c9d12f2594586d4825e4058799d87
Nitesh Ahiniya <niteshahiniya2.0@gmail.com>
2025-09-08 12:42:57 +0530

    Update Service.jsx

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -11,7 +11,7 @@ const Service = () => {
             <div className='grid grid-cols-1 md:grid-cols-4 gap-8'>
                 <div className='w-[100%] h-80 border border-[#9824f4]/80 rounded-2xl shadow-2xl p-10'>
                 <div className='w-20 h-20 rounded-full bg-amber-600/20 flex items-center justify-center mb-6'>
-                    <img src="public/images/Free WHM & cPanel.png" alt="" />
+                    <img src="/public/images/Free WHM & cPanel.png" alt="cPanel" />
                 </div>
                 <h3 className='text-xl text-dark font-bold mb-6 '>Free WHM & cPanel</h3>
                 <p className='text-dark/80'>We guarantee it you don't have to worry about it.</p>
@@ -21,7 +21,7 @@ const Service = () => {
 
                 <div className='w-[100%] h-80 border border-[#9824f4]/80 rounded-2xl shadow-2xl p-10'>
                 <div className='w-20 h-20 rounded-full  bg-amber-600/20 flex items-center justify-center mb-6'>
-                    <img src="public/images/Performance_Optimized_icon.png" alt="" />
+                    <img src="/public/images/Performance_Optimized_icon.png" alt="Performance" />
                 </div>
                 <h3 className='text-xl text-dark font-bold mb-6 '>Performance Optimized</h3>
                 <p className='text-dark/80'>If your website is slow or down then you losing Customers.</p>
@@ -30,7 +30,7 @@ const Service = () => {
 
                 <div className='w-[100%] h-80 border border-[#9824f4]/80 rounded-2xl shadow-2xl p-10'>
                 <div className='w-20 h-20 rounded-full  bg-amber-600/20 flex items-center justify-center mb-6'>
-                    <img src="public/images/Super Easy to Use-icon.png" alt="" />
+                    <img src="/public/images/Super Easy to Use-icon.png" alt="" />
                 </div>
                 <h3 className='text-xl text-dark font-bold mb-6 '>Super Easy to Use</h3>
                 <p className='text-dark/80'>Our custom control panel to use and removes the headache.</p>
@@ -39,7 +39,7 @@ const Service = () => {
 
                 <div className='w-[100%] h-80 border border-[#9824f4]/80 rounded-2xl shadow-2xl p-10'>
                 <div className='w-20 h-20 rounded-full  bg-amber-600/20 flex items-center justify-center mb-6'>
-                    <img src="public/images/Expert_Support-icon.png" alt="" />
+                    <img src="/public/images/Expert_Support-icon.png" alt="Expert Support" />
                 </div>
                 <h3 className='text-xl text-dark font-bold mb-6 '>24/7 Expert Support</h3>
                 <p className='text-dark/80'>Our custom control panel to use and removes the headache</p>
